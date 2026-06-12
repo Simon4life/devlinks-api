4def2d87f442fe5291cc6f5df4ef274869c17c74
Ogar Simon <ogarsimon0000@gmail.com>
2026-06-12 12:44:40 +0100

    initial commit

--- frontend/src/utils/customFetch.jsx
+++ frontend/src/utils/customFetch.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const customFetch = () => {
   const accessToken = JSON.parse(localStorage.getItem("user")).accessToken;
   return axios.create({
-    baseURL: "https://devlinks-api-1g68.onrender.com",
+    baseURL: "https://devlinks-api-bcmv.onrender.com",
     headers: {
       "Content-type": "application/json; charset=UTF-8",
       "Authorization": `Bearer ${accessToken}`
